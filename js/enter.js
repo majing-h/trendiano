ed4e6f806bb6d7bf5b438a8623f7c576d49ca61f
mj <2113001473@qq.com>
2021-01-28 16:50:45 +0800

    这是第一次上传的

---
+++ js/enter.js
@@ -0,0 +1,5 @@
+// 给登录按钮绑定事件
+$("#enter").click(function(){
+    let http = new XMLHttpRequest();
+    http.open("get",`http://192.168.137.1/login?`)
+})
---
+++ js/page.js
@@ -0,0 +1,71 @@
+// 精选货品
+$("#nav .index_01").mouseenter(function(){
+    $("#nav_1").css("display","block")
+})
+$("#nav .index_01").mouseleave(function(){
+    $("#nav_1").css("display","none")
+})
+// 到店新品
+$("#nav .index_02").mouseenter(function(){
+    $("#nav_2").css("display","block")
+})
+$("#nav .index_02").mouseleave(function(){
+    $("#nav_2").css("display","none")
+})
+// 跨界合作
+$("#nav .index_03").mouseenter(function(){
+    $("#nav_3").css("display","block")
+})
+$("#nav .index_03").mouseleave(function(){
+    $("#nav_3").css("display","none")
+})
+// 特惠专区
+$("#nav .index_04").mouseenter(function(){
+    $("#nav_4").css("display","block")
+})
+$("#nav .index_04").mouseleave(function(){
+    $("#nav_4").css("display","none")
+})
+// 服装
+$("#nav .index_05").mouseenter(function(){
+    $("#nav_5").css("display","block")
+})
+$("#nav .index_05").mouseleave(function(){
+    $("#nav_5").css("display","none")
+})
+// 鞋
+$("#nav .index_06").mouseenter(function(){
+    $("#nav_6").css("display","block")
+})
+$("#nav .index_06").mouseleave(function(){
+    $("#nav_6").css("display","none")
+})
+
+// 四张小图
+$("#middle .picture_01").mouseenter(function(){
+    $(".middle_shaow1").fadeIn("slow")
+})
+$("#middle .picture_01").mouseenter(function(){
+    $(".middle_shaow1").fadeOut("slow")
+})
+// 2
+$("#middle .picture_02").mouseenter(function(){
+    $(".middle_shaow2").fadeIn("slow")
+})
+$("#middle .picture_02").mouseenter(function(){
+    $(".middle_shaow2").fadeOut("slow")
+})
+// 3
+$("#middle .picture_03").mouseenter(function(){
+    $(".middle_shaow3").fadeIn("slow")
+})
+$("#middle .picture_03").mouseenter(function(){
+    $(".middle_shaow3").fadeOut("slow")
+})
+// 4
+$("#middle .picture_04").mouseenter(function(){
+    $(".middle_shaow4").fadeIn("slow")
+})
+$("#middle .picture_04").mouseenter(function(){
+    $(".middle_shaow4").fadeOut("slow")
+})
